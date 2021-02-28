e84c58f4e2372e239e4f0a1e09b364241acebbef
JanJu <janjunklewitz@gmail.com>
2021-02-28 11:01:39 +0100

    adjusted messages and added exercises

--- Workout.ps1
+++ Workout.ps1
@@ -1,5 +1,5 @@
 function voice_and_text_msg($msg){
-	Write-Host "##### $msg ##############" -ForegroundColor blue
+	Write-Host "$msg" -ForegroundColor green
 	
 	$voice = New-Object -ComObject Sapi.spvoice
 	$voice.rate = 0
@@ -25,44 +25,51 @@ function done (){
 	voice_and_text_msg "You finished you Workout!!"
 }
 
+# focus on chest and core
 function workout_1(){		
-	exercise 60 "jumping jacks"
-	exercise 60 "squats"
-	exercise 60 "lunges"
-	exercise 60 "leg raise"
+	exercise 60 "high knees"
+	exercise 60 "bridges"
+	exercise 60 "push ups"
+	exercise 60 "bicycle crunches"
 	exercise 60 "handstand"
 	exercise 60 "walking push ups"
-	exercise 60 "crunsh"
-	exercise 60 "burpee"
+	exercise 60 "crunch"
+	exercise 60 "plank left"
+	exercise 60 "plank right"
 	exercise 60 "superman"
-	exercise 60 "opposite leg and arm rise"
-	exercise 60 "plank"
 }
 
-function workout_2(){			
+function exercise_collection(){			
 	exercise 60 "jumping jacks"
 	exercise 60 "squats"
 	exercise 60 "lunges"
 	exercise 60 "leg raise"
 	exercise 60 "handstand"
 	exercise 60 "walking push ups"
-	exercise 60 "crunsh"
+	exercise 60 "crunch"
 	exercise 60 "burpee"
 	exercise 60 "superman"
 	exercise 60 "opposite leg and arm rise"
-	exercise 60 "plank"
+	exercise 60 "elbow plank"
+	exercise 60 "sit-ups"
+	exercise 60 "plank left"
+	exercise 60 "plank right"
+	exercise 60 "reverse crunches"
+	exercise 60 "brideges (glute bride)"
+	exercise 60 "bicycle crunches"
+	exercise 60 "bicycle crunches"
 }
 
 $t_start = Get-Date
 
 
 workout_1
-# workout_2
 
 $t_end = Get-Date
 $dt = $t_end - $t_start
-$dt.Seconds
-voice_and_text_msg "##### Workout lasted for $dt. ##############" -ForegroundColor green
+$seconds = $dt.Seconds
+$minutes = $dt.Minutes
+voice_and_text_msg "Congratulations you finished your Workout. It lasted for $minutes minutes and $seconds seconds." 
 
 done
 pause
